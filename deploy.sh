eece5bfb566c7ffc87152818655be7e1cd8cfece
0x101008 <shtnb@outlook.com>
2026-03-15 13:53:46 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env sh
+git pull
+git status
+git add .
+git commit . -m "update"
+git push -u origin main
+npm install
+npm run build
+
+set -e
+cd dist
+echo "已成功进入目录打包...正在进行打包"
+zip -r ../zip/dist.zip ./
+echo "已经成功打包"
+
+echo "***** 上传中 *****"
+scp -v -r ../zip/dist.zip root@154.37.212.50:/www/wwwroot/bloggersht.com.cn/
+echo "***** 成功上传 *****"
+# del /q /f ../zip/dist.zip
+echo "***** 进入服务器，触发远端程序 *****"
+ssh root@154.37.212.50 "sh /www/wwwroot/bash/autounzip.sh"
+echo "***** 传输完毕*****"
