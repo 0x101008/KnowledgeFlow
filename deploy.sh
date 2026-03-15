56f1228477a3899976765c165e89ed3bcfee1255
0x101008 <shtnb@outlook.com>
2026-03-15 13:58:05 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,13 @@ npm run build
 set -e
 cd dist
 echo "已成功进入目录打包...正在进行打包"
-7z -r ../zip/dist.zip ./
+7z a ../zip/dist.zip ./
 echo "已经成功打包"
 
 echo "***** 上传中 *****"
-scp -v -r ../zip/dist.zip root@154.37.212.50:/www/wwwroot/bloggersht.com.cn/
+scp -v -r ../zip/dist.zip root@154.37.212.50:/www/wwwroot/flow.foxora.cn/
 echo "***** 成功上传 *****"
 # del /q /f ../zip/dist.zip
 echo "***** 进入服务器，触发远端程序 *****"
-ssh root@154.37.212.50 "sh /www/wwwroot/bash/autounzip.sh"
+ssh root@154.37.212.50 "sh /www/wwwroot/bash/autodeploy.sh"
 echo "***** 传输完毕*****"
